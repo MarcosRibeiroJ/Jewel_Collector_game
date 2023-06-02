438d83a4726d11af8c9b9ec0be8894a4bbdf8397
Marcos Ribeiro <marcossrj90@gmail.com>
2023-06-02 13:38:40 -0300

    Documentando classe Water

--- JewelCollectorProject/Obstacles/Water.cs
+++ JewelCollectorProject/Obstacles/Water.cs
@@ -5,8 +5,15 @@ using System.Threading.Tasks;
 
 namespace JewelCollectorProject.Obstacles
 {
+    /// <summary>
+    /// Classe que no jogo será usada como obstáculo para Robot.
+    /// </summary>
     public class Water : Cell
     {
+        /// <summary>
+        /// Reescrita do método ToString herdado da classe abstrata Cell
+        /// </summary>
+        /// <returns>Retorna a impressão visual de água no jogo.</returns>
         public override string ToString()
         {
             return " ## ";
