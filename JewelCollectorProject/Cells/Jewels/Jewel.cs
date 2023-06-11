053735b6d6c57d9377ac4502da759af2dba6e036
Marcos Ribeiro <marcossrj90@gmail.com>
2023-06-10 22:08:10 -0300

    Edicao documentacao Jewel

--- JewelCollectorProject/Cells/Jewels/Jewel.cs
+++ JewelCollectorProject/Cells/Jewels/Jewel.cs
@@ -7,11 +7,17 @@ namespace JewelCollectorProject.Cells.Jewels
 {
     /// <summary>
     /// Classe abstrata de Jewel que herda de Cell
-    /// Possui apenas um atributo do tipo inteiro representando o valor de cada Jewel
+    /// Possui apenas um atributo do tipo inteiro representando o valor de cada Jewel.
     /// </summary>
     public abstract class Jewel : Cell
     {
         public int JewelValue {get;}
+        /// <summary>
+        /// Construtor de Jewel.
+        /// O construtor tem a finalidade de receber um inteiro que será utilizado em JewelValue.
+        /// Esse valor será passado no momento que outros elementos que herdam de Jewel forem construídos.
+        /// </summary>
+        /// <param name="value">valor do tipo int que atribuído a JewelValue</param>
         public Jewel(int value)
         {
             this.JewelValue = value;
