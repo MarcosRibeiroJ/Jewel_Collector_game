00f73be9c49ad1e6ee523260938d75bff4317eb6
Marcos Ribeiro <marcossrj90@gmail.com>
2023-06-11 18:35:13 -0300

    Documentacao classe JewelCollector

--- JewelCollectorProject/JewelCollector.cs
+++ JewelCollectorProject/JewelCollector.cs
@@ -1,10 +1,17 @@
 ﻿using JewelCollectorProject.Maps;
 namespace JewelCollectorProject
 {
+    /// <summary>
+    /// Classe principal do programa.
+    /// </summary>
     public class JewelCollector
     {
+        /// <summary>
+        /// Método principal do programa.
+        /// Instancia um objeto do tipo Game e executa o método startGame para iniciar o jogo.
+        /// </summary>
         public static void Main() {
-            Game newGame = new Game(11);
+            Game newGame = new Game();
             newGame.startGame();
         }
     }
