a40e4a85f4e5aa6f840638c42b74411056c084b2
Marcos Ribeiro <marcossrj90@gmail.com>
2023-06-11 10:36:02 -0300

    Documentacao classe Atomic

--- JewelCollectorProject/Cells/Obstacles/Atomic.cs
+++ JewelCollectorProject/Cells/Obstacles/Atomic.cs
@@ -5,14 +5,25 @@ using System.Threading.Tasks;
 
 namespace JewelCollectorProject.Cells.Obstacles
 {
+    /// <summary>
+    /// Classe que herda da classe abstrata Cell.
+    /// Esse elemento será responsável por causar dano ao robô durante o jogo.
+    /// Possui dois atributos do tipo inteiro, um chamado DamageArea que representa o dano quando robô estiver ao seu lado em qualquer uma das quatro direções.
+    /// E o outro chamado Damage, representa o dano causado ao robô quando ele atravessa diretamente o obstáculo.
+    /// </summary>
     public class Atomic : Cell
     {
         public static int DamageArea {get;} = 10;
         public static int Damage {get;} = 30;
         
+        /// <summary>
+        /// Sobrescrita do método ToString herdado da classe abstrata Cell.
+        /// </summary>
+        /// <returns>Retorna a impressão visual de Atomic no jogo formatada na cor de fundo amarelo escuro e cor de texto preto.</returns>
         public override string ToString()
         {
             Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.ForegroundColor = ConsoleColor.Black;
             return " !! ";
         }
     }
