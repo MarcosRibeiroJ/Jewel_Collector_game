dbbc869f12d9f9c16518a4dfa86765e68d29a0c6
Marcos Ribeiro <marcossrj90@gmail.com>
2023-06-11 10:21:49 -0300

    Documentacao classe RedJewel

--- JewelCollectorProject/Cells/Jewels/RedJewel.cs
+++ JewelCollectorProject/Cells/Jewels/RedJewel.cs
@@ -5,10 +5,21 @@ using System.Threading.Tasks;
 
 namespace JewelCollectorProject.Cells.Jewels
 {
+    /// <summary>
+    /// Classe que herda de Jewel.
+    /// Possui o atributo herdado JewelValue.
+    /// </summary>
     public class RedJewel : Jewel
     {
+        /// <summary>
+        /// Construtor de RedJewel que acessa o construtor da classe mãe Jewel, passando como parâmetro um inteiro que será utilizado em JewelValue.
+        /// </summary>
         public RedJewel() : base(100){}
 
+        /// <summary>
+        /// Sobrescrita do método ToString herdado da classe abstrata Cell.
+        /// </summary>
+        /// <returns>Retorna a impressão visual de RedJewel no jogo formatada na cor de fundo vermelho e cor de texto preto.</returns>
         public override string ToString()
         {
             Console.BackgroundColor = ConsoleColor.Red;
