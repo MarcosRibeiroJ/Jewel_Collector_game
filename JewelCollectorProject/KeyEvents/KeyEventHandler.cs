b809817140a423bc5f8e45a08cfccdad3a4301f0
Marcos Ribeiro <marcossrj90@gmail.com>
2023-06-11 18:07:36 -0300

    Documentacao delegate KeyPressedEventHandler

--- JewelCollectorProject/KeyEvents/KeyEventHandler.cs
+++ JewelCollectorProject/KeyEvents/KeyEventHandler.cs
@@ -2,5 +2,9 @@ using System;
 
 namespace JewelCollectorProject.KeyEvents
 {
+    /// <summary>
+    /// Delegate criado para tratar os eventos de teclas pressionadas durante o jogo.
+    /// </summary>
+    /// <param name="keyPressed">Recebe um string com a tecla que foi pressionada no teclado.</param>
     public delegate void KeyPressedEventHandler(string keyPressed);
 }
