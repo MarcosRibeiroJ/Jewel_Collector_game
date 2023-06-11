4848e1ab19a3ea016915a3bca82f67eb4ebbc6c2
Marcos Ribeiro <marcossrj90@gmail.com>
2023-06-11 17:55:00 -0300

    Documentacao classe Cell

--- JewelCollectorProject/Cells/Cell.cs
+++ JewelCollectorProject/Cells/Cell.cs
@@ -5,9 +5,16 @@ using System.Threading.Tasks;
 
 namespace JewelCollectorProject.Cells
 {
+    /// <summary>
+    /// Classe abstrata Cell que representa uma célula dentro do jogo.
+    /// </summary>
     public abstract class Cell
     {
+        /// <summary>
+        /// Método abstrato ToString que foi sobrescrito da classe Object.
+        /// Quem herdar dessa classe é obrigado a sobrescrever esse método, garantindo que todo elemento do jogo terá uma representação gráfica.
+        /// </summary>
+        /// <returns>string com a representação gráfica do elemento do jogo.</returns>
         public abstract override string ToString();
-        //public abstract bool isRechargeable();
     }
 }
