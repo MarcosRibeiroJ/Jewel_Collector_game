4c4e24f5c48a3dac9f0c947642522a87153ddc96
Marcos Ribeiro <marcossrj90@gmail.com>
2023-06-11 18:02:15 -0300

    Documentacao classe Empty

--- JewelCollectorProject/Cells/Empty.cs
+++ JewelCollectorProject/Cells/Empty.cs
@@ -5,11 +5,19 @@ using System.Threading.Tasks;
 
 namespace JewelCollectorProject.Cells
 {
+    /// <summary>
+    /// Classe que representa um espaço vazio que o robô pode se movimentar.
+    /// </summary>
     public class Empty : Cell
     {
+        /// <summary>
+        /// Sobrescrita do método ToString herdado da classe abstrata Cell.
+        /// </summary>
+        /// <returns>Retorna a impressão visual de uma célula vazia no jogo formatada na cor de fundo cinza escuro e cor de texto branco.</returns>
         public override string ToString()
         {
             Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.White;
             return " -- ";
         }
     }
