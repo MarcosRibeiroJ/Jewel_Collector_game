13321df6ec90c1cf50952c27bc011f2d6c6f8d59
Marcos Ribeiro <marcossrj90@gmail.com>
2023-06-14 10:58:21 -0300

    Gerado nova documentacao apos alteracao classe Game

--- JewelCollectorProject/html/search/all_4.js
+++ JewelCollectorProject/html/search/all_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['game_0',['Game',['../class_jewel_collector_project_1_1_game.html#a1ca37a5e037c0776ff27f20bae99dd59',1,'JewelCollectorProject.Game.Game()'],['../class_jewel_collector_project_1_1_game.html#a84a2b597e9008b5993e8fcf7b0c4b517',1,'JewelCollectorProject.Game.Game(int dimension)'],['../class_jewel_collector_project_1_1_game.html',1,'JewelCollectorProject.Game']]],
+  ['game_0',['Game',['../class_jewel_collector_project_1_1_game.html#a1ca37a5e037c0776ff27f20bae99dd59',1,'JewelCollectorProject.Game.Game()'],['../class_jewel_collector_project_1_1_game.html',1,'JewelCollectorProject.Game']]],
   ['greenjewel_1',['GreenJewel',['../class_jewel_collector_project_1_1_cells_1_1_jewels_1_1_green_jewel.html#affa3e1ac9f86fe9314f57f74f32ca6f7',1,'JewelCollectorProject.Cells.Jewels.GreenJewel.GreenJewel()'],['../class_jewel_collector_project_1_1_cells_1_1_jewels_1_1_green_jewel.html',1,'JewelCollectorProject.Cells.Jewels.GreenJewel']]]
 ];
